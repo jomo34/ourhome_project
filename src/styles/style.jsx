dbb3b0030f28a84c4e8846b73c59b9f0617b9362
jomo34 <trillion32@naver.com>
2024-01-02 00:40:28 +0900

    styles: 전체 스타일 설정

---
+++ src/styles/style.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+export const Wrapper = styled.div`
+  display: flex;
+  background-color: white;
+  min-height: 100vh;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`
