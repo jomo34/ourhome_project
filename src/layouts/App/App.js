49bf244d274b33a656bfc21ef3a7c63a4f54f316
jomo34 <trillion32@naver.com>
2024-01-01 22:50:30 +0900

    feat: 기본 경로 설정

--- src/layouts/App/App.js
+++ src/layouts/App/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import loadable from '@loadable/component';
 import './App.css';
 
+const ProductListPage = loadable(() => import(''))
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
